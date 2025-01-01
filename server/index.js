fdfe965b170f38f55ad216b8ae975b19280f8070
Samyak <samyak.coll1905@gmail.com>
2025-01-01 14:47:54 +0530

    setup basic server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const {Server} = require("socket.io");
+app.use(cors());
+app.use(express.json());
 
 const app = express();
+const server = http.createServer(app);
 
-app.listen(2000, ()=>{
+const io = new Server(server, {
+    cors:{
+        origin : 'http://localhost:5173/',
+        methods : ['GET','POST']
+    }
+})
+
+io.on("connection", (socket)=>{
+    console.log("user joined with socket id", socket.id);
+})
+
+
+server.listen(2000, ()=>{
     console.log("server runnong on http://localhost/2000");
 })
